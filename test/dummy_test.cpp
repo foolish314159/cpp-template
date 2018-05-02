1fc3a27c25668f27ff6fd7df1b412320c62290b2
foolish314159 <foolish1337@gmx.de>
2018-05-02 21:50:14 +0200

    Add .travis.yml

--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -4,13 +4,9 @@
 TEST(AddTest, PositiveNumbers)
 {
   EXPECT_EQ(8, Add(2, 6));
-  EXPECT_EQ(4, Add(1, 3));
-  EXPECT_EQ(10, Add(5, 5));
 }
 
 TEST(AddTest, NegativeNumbers)
 {
   EXPECT_EQ(-4, Add(-2, -2));
-  EXPECT_EQ(-9, Add(-3, -6));
-  EXPECT_EQ(-3, Add(-2, -1));
 }
